71832b1f6d019abedab7141217a02d92fa4f96e9
Joel Andrade <joelcesar09@gmail.com>
2023-06-15 20:28:33 -0300

    Desafio 1 da mentoria com o Tiago Aguiar

---
+++ Encontro1/Desafio1/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] numbers = new int[10]; 
+int contador = 0;  
+
+Console.WriteLine("Digite 10 números abaixo:");  
+
+for (int i = 0; i < numbers.Length; i++) 
+{   
+  numbers[i] = int.Parse(Console.ReadLine()); 
+}  
+
+foreach (int number in numbers) 
+{   
+  if (number % 2 != 0)   
+  {     
+    Console.Write($"{number}, ");    
+    contador++;   
+  }
+}  
+
+Console.WriteLine($"Total de números ímpares: {contador}");
