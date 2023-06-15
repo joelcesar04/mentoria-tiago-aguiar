c67777398fbbddaeea9b2fb94ae7f7ff93a64ec9
Joel Andrade <joelcesar09@gmail.com>
2023-06-15 20:28:00 -0300

    Utilização da palavra reservada 'return' na aplicação

---
+++ Encontro1/Return/Program.cs
@@ -0,0 +1,10 @@
+﻿static int EncontrarNumero(int[] numeros, int alvo)
+{
+    for (int i = 0; i < numeros.Length; i++)
+    {
+        if (numeros[i] == alvo)
+            return i; // Retorna o índice se o número for encontrado
+    }
+
+    return -1; // Retorna -1 se o número não for encontrado
+}
